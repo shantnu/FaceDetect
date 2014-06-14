8d465477ad87f0b06529c37590c40b0770fd45f3
shantnu <a@a.com>
2014-06-14 21:25:44 +0100

    Rough Python code to detect faces

---
+++ face_detect.py
@@ -0,0 +1,24 @@
+import cv2
+
+imagePath = "./abba.png"
+cascPath = "./haarcascade_frontalface_default.xml"
+
+# Create the haar cascade
+faceCascade = cv2.CascadeClassifier(cascPath)
+
+# Read the image
+image = cv2.imread(imagePath)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# Detect faces in the image
+faces = faceCascade.detectMultiScale(gray, scaleFactor = 1.1, minNeighbors = 5, minSize = (30,30), flags = cv2.cv.CV_HAAR_SCALE_IMAGE)
+
+print "Found %d faces " % len(faces)
+
+# Draw a rectangle around the faces
+for (x, y, w ,h) in faces:
+    cv2.rectangle(image, (x,y), (x+w, y+h), (0,255,0) , 2)
+
+cv2.imshow("Faces  found" ,image)
+cv2.waitKey(0)
+
