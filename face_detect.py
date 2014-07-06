e91c9926970e02de75020a8d1633fa5f0eca9a0f
Michael Herman <hermanmu@gmail.com>
2014-07-05 21:32:29 -0700

    Update face_detect.py

--- face_detect.py
+++ face_detect.py
@@ -1,7 +1,9 @@
 import cv2
+import sys
 
-imagePath = "./abba.png"
-cascPath = "./haarcascade_frontalface_default.xml"
+# Get user supplied values
+imagePath = sys.argv[1]
+cascPath = sys.argv[2]
 
 # Create the haar cascade
 faceCascade = cv2.CascadeClassifier(cascPath)
@@ -11,14 +13,19 @@ image = cv2.imread(imagePath)
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 # Detect faces in the image
-faces = faceCascade.detectMultiScale(gray, scaleFactor = 1.1, minNeighbors = 5, minSize = (30,30), flags = cv2.cv.CV_HAAR_SCALE_IMAGE)
+faces = faceCascade.detectMultiScale(
+    gray,
+    scaleFactor=1.1,
+    minNeighbors=5,
+    minSize=(30, 30),
+    flags = cv2.cv.CV_HAAR_SCALE_IMAGE
+)
 
-print "Found %d faces " % len(faces)
+print "Found {0} faces!".format(len(faces))
 
 # Draw a rectangle around the faces
-for (x, y, w ,h) in faces:
-    cv2.rectangle(image, (x,y), (x+w, y+h), (0,255,0) , 2)
+for (x, y, w, h) in faces:
+    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-cv2.imshow("Faces  found" ,image)
+cv2.imshow("Faces found", image)
 cv2.waitKey(0)
-
